6b5d6cabc8fb68b17e1caeed92122e554599ac7f
Breno G. de Oliveira <garu@cpan.org>
2010-10-14 00:10:25 +0000

    Import of GARU/Acme-CPANAuthors-Brazilian-0.12 from CPAN.
    gitpan-cpan-distribution: Acme-CPANAuthors-Brazilian
    gitpan-cpan-version:      0.12
    gitpan-cpan-path:         GARU/Acme-CPANAuthors-Brazilian-0.12.tar.gz
    gitpan-cpan-author:       GARU
    gitpan-cpan-maturity:     released

--- lib/Acme/CPANAuthors/Brazilian.pm
+++ lib/Acme/CPANAuthors/Brazilian.pm
@@ -3,7 +3,7 @@ package Acme::CPANAuthors::Brazilian;
 use strict;
 use warnings;
 
-our $VERSION = '0.11';
+our $VERSION = '0.12';
 
 use Acme::CPANAuthors::Register (
     AGUIMARA    => 'Andre Guimaraes',
@@ -35,6 +35,7 @@ use Acme::CPANAuthors::Register (
     GEOJS       => 'Geovanny Junio',
     GMPASSOS    => 'Graciliano Monteiro Passos',
     GNUSTAVO    => 'Gustavo Leite de Mendonça Chaves',
+    HERNAN      => 'Hernan Lopes',
     HODEL       => 'Paul Hodel',
     IZUT        => 'Igor Sutton',
     JEAN        => 'Jean Carlo',
